92a61248bd8c4e8e86d0cec3519aa72a399e5bee
FabianVegaA <fabianveal@gmail.com>
2021-02-07 00:36:05 -0300

    It was added the built-in function "printLn"

--- sigmaF/builtins.py
+++ sigmaF/builtins.py
@@ -4,9 +4,14 @@ from typing import (
 )
 
 from sigmaF.object import (
+    Boolean,
     Builtin,
+    Float,
+    Function,
     Error,
     Integer,
+    ValueList,
+    Null,
     Object,
     String
 )
@@ -25,6 +30,35 @@ def length(*args: Object) -> Object:
         return Error(_UNSUPPORTED_ARGUMENT_TYPE.format('length', args[0].type().name))
 
 
+def printLn(*args: Object) -> Object:
+    if len(args) != 1:
+        return Error(_WRONG_NUMBER_OF_ARGS.format(len(args), 1))
+    else:
+        if type(args[0]) == String:
+            argument = cast(String, args[0])
+            print(argument.inspect())
+        elif type(args[0]) == Integer:
+            argument = cast(Integer, args[0])
+            print(argument.inspect())
+        elif type(args[0]) == Float:
+            argument = cast(Float, args[0])
+            print(argument.inspect())
+        elif type(args[0]) == ValueList:
+            argument = cast(ValueList, args[0])
+            print(argument.inspect())
+        elif type(args[0]) == Boolean:
+            argument = cast(Boolean, args[0])
+            print(argument.inspect())
+        elif type(args[0]) == Function:
+            argument = cast(Function, args[0])
+            print(argument.inspect())
+        else:
+            return Error(_UNSUPPORTED_ARGUMENT_TYPE.format('printLn', args[0].type().name))
+
+        return Null()
+
+
 BUILTIN: Dict[str, Builtin] = {
-    'length': Builtin(fn=length)
+    'length': Builtin(fn=length),
+    'printLn': Builtin(fn=printLn),
 }
