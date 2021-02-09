192b20f8f3a5116a96def3ac6194b705aeec7923
FabianVegaA <fabianveal@gmail.com>
2021-02-09 19:59:21 -0300

    It was added builtin parse of int to float and str and list and builtin pow that allow operations between int and float

--- sigmaF/builtins.py
+++ sigmaF/builtins.py
@@ -1,3 +1,4 @@
+import math
 from typing import (
     cast,
     Dict,
@@ -21,6 +22,7 @@ from sigmaF.object import (
 
 _WRONG_NUMBER_OF_ARGS = 'Incorrect Number of arguments for length, it was received {} arguments, and is needed only {}'
 _UNSUPPORTED_ARGUMENT_TYPE = 'Argument to {} without support, it was received a {}'
+_PARSE_WRONG = 'It is not possible to parser since {} to {}'
 
 
 def length(*args: Object) -> Object:
@@ -76,7 +78,7 @@ def printLn(*args: Object) -> Object:
         return Null()
 
 
-def negation_bolean(*args: Object):
+def negation_bolean(*args: Object) -> Object:
     if len(args) != 1:
         return Error(_WRONG_NUMBER_OF_ARGS.format(len(args), 1))
     else:
@@ -90,8 +92,69 @@ def negation_bolean(*args: Object):
             return Error(_UNSUPPORTED_ARGUMENT_TYPE.format('not', args[0].type().name))
 
 
+def pow_impure(*args: Object) -> Object:
+    if len(args) != 2:
+        return Error(_WRONG_NUMBER_OF_ARGS.format(len(args), 2))
+    else:
+        type_radicant: Type = type(args[0])
+        type_index: Type = type(args[1])
+        radicant: Optional[Union[Integer, Float]] = None
+        index: Optional[Union[Integer, Float]] = None
+
+        if type_radicant == Integer and type_index == Integer:
+            radicant = cast(Integer, args[0])
+            index = cast(Integer, args[1])
+
+            root_math = math.pow(radicant.value, 1 / index.value)
+            return Float(root_math)
+        elif type_radicant == Float and type_index == Float:
+            radicant = cast(Float, args[0])
+            index = cast(Float, args[1])
+
+            root_math = math.pow(radicant.value, 1 / index.value)
+            return Float(root_math)
+        elif type_radicant == Float and type_index == Integer:
+            radicant = cast(Float, args[0])
+            index = cast(Integer, args[1])
+
+            root_math = math.pow(radicant.value, 1 / index.value)
+            return Float(root_math)
+        elif type_radicant == Integer and type_index == Float:
+            radicant = cast(Integer, args[0])
+            index = cast(Float, args[1])
+
+            root_math = math.pow(radicant.value, 1 / index.value)
+            return Float(root_math)
+        else:
+            return Error(_UNSUPPORTED_ARGUMENT_TYPE.format('pow', args[0].type().name))
+
+
+def parse(*args: Object) -> Object:
+    if len(args) != 2:
+        return Error(_WRONG_NUMBER_OF_ARGS.format(len(args), 2))
+
+    elif not args[0].type() == Object and args[1].type() == String:
+        types = [arg.type().name for arg in args]
+        Error(_UNSUPPORTED_ARGUMENT_TYPE.format(
+            'pow', f'{types[0]} and {types[1]}'))
+
+    else:
+        type_parse: str = cast(String, args[1]).inspect()
+
+        if type(args[0]) == Integer and type_parse == "float":
+            arg = cast(Integer, args[0])
+            return Float(float(arg.value))
+        elif type(args[0]) == String and type_parse == "list":
+            arg = cast(String, args[0])
+            return ValueList([String(value_list) for value_list in list(arg.value)])
+        else:
+            return Error(_PARSE_WRONG.format(args[0].type().name, args[1].inspect()))
+
+
 BUILTIN: Dict[str, Builtin] = {
     'length': Builtin(fn=length),
     'printLn': Builtin(fn=printLn),
     'not': Builtin(fn=negation_bolean),
+    'pow': Builtin(fn=pow_impure),
+    'parse': Builtin(fn=parse),
 }
