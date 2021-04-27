155e1bcddf8bead65295ee219a870ef779c1780d
FabianVegaA <fabianveal@gmail.com>
2021-04-26 22:20:22 -0400

    Modified the cover to console, added head and cover option

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import sys
+import yaml
+import re
 
 from typing import (
     Optional,
@@ -22,27 +24,79 @@ SIGMAF: str = """
 """
 
 
-def main(arg=None) -> None:
+def show_cover(version):
     global SIGMAF
     print('-'*106)
     print(
-        f'\n\nWelcome to SigmaF, the Program Language of the future for the Programming Functional and a lot more\n\n{SIGMAF}')
+        f'\n\nWelcome to SigmaF v{version}, the Program Language of the future for the Programming Functional and a lot more\n\n{SIGMAF}')
     print('-'*106)
-    if arg is None:
-        start_repl()
+
+
+def show_head(version):
+    print(f'SigmaF v{version} | Exit: exit() ')
+
+
+def get_configs():
+    with open('configs.yaml', 'r') as fin:
+        configs = yaml.load(fin, Loader=yaml.FullLoader)
+    return dict(configs)
+
+
+def presentation_config(configs, params, exe_file=False):
+    version = configs['version']
+    if not params is None:
+        if exe_file:
+            if '-cover' in params:
+                show_cover(version)
+                return
+            show_head(version)
+        else:
+            if not '-ncover' in params:
+                show_cover(version)
+                return
+            show_head(version)
     else:
-        with open(arg, mode='r', encoding='utf-8') as fin:
+        if exe_file:
+            show_head(version)
+        else:
+            show_cover(version)
+
+
+def main(path=None, params=None) -> None:
+    configs = get_configs()
+    if path is None:
+        presentation_config(configs, params, exe_file=False)
+        start_repl()
+    elif not path is None:
+        presentation_config(configs, params, exe_file=True)
+
+        with open(path, mode='r', encoding='utf-8') as fin:
             lines = fin.readlines()
-        start_repl('\n'.join(lines))
+        fin = '\n'.join(lines)
+
+        start_repl(fin)
+
+
+def filter_path_params(args):
+    path = list(filter(lambda s: not re.match('(\S+?\.sf$)', s) is None, args))
+    params = list(filter(lambda s: s.startswith('-'), args))
+    if len(path) > 0:
+        path = path[0]
+    else:
+        path = None
+
+    if len(params) == 0:
+        params = None
+
+    return path, params
 
 
 if __name__ == '__main__':
     args: List[str] = sys.argv
-    arg: Optional[str]
 
     if len(args) > 1:
-        arg = args[1]
+        path, params = filter_path_params(args)
     else:
-        arg = None
+        path, params = None, None
 
-    main(arg)
+    main(path, params)
